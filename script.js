172b9c4c67b16b6bb156381259eb9797effaa7cb
Prajwal Behalpande <94629211+Prajwalbehalpande@users.noreply.github.com>
2023-07-25 12:15:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  // The existing check functionality remains unchanged
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#D36600';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Attach a click event handler to the element with the 'again' class
+document.querySelector('.again').addEventListener('click', function () {
+  // 1. Restore initial values of score and secretNumber variables
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // 2. Restore initial conditions of message, number, score, and guess input field
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  // 3. Restore original background color (#000) and number width (15rem)
+  document.querySelector('body').style.backgroundColor = '#000';
+  document.querySelector('.number').style.width = '15rem';
+});
